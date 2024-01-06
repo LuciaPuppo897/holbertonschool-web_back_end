1c26ac178dc80aa87f31f2431a3c7a4ea49ffa9a
LuciaPuppo897 <alerome897@gmail.com>
2024-01-05 21:00:33 -0800

    add task 1 n 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" floor function with annoatation float, and int"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the given float."""
+    return math.floor(n)
