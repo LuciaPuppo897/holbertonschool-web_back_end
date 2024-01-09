b592d25621d514653b3bbd10a294238d8ddff30e
LuciaPuppo897 <alerome897@gmail.com>
2024-01-09 14:29:09 -0800

    feat: add task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""calls async_generator and do the same but with args"""
+
+import asyncio
+import random
+async_generator = __import__('1-async_comprehension').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Asynchronous coroutine that collects 10 random numbers using async"""
+    result = [num async for num in async_generator()]
+    return result
