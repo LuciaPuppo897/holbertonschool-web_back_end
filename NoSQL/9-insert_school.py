cb6535fe1d7a85e5d620580e9851938c4cfc0c57
LuciaPuppo897 <alerome897@gmail.com>
2024-01-12 14:13:24 -0800

    add task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that insert in mongo"""
+    document_id = mongo_collection.insert_one(kwargs).inserted_id
+    return document_id
