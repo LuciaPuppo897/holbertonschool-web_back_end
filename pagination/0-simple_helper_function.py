77645c3a9642bd88da5c21d3f578f7886dd5b568
LuciaPuppo897 <alerome897@gmail.com>
2024-01-12 14:51:57 -0800

    fix: task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,14 +3,12 @@
 function named index_range that takes two integer arguments page and page_size
 """
 
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int , page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end indexes for a given page and page size.
     """
-    if page <= 0 or page_size <= 0:
-        raise ValueError("Page and page_size must be greater than zero.")
-
     start_index = (page - 1) * page_size
     end_index = start_index + page_size - 1
 
