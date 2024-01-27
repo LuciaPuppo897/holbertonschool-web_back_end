6d6c99aafbd191844e83e311a484f5088ce4ed28
LuciaPuppo897 <alerome897@gmail.com>
2024-01-27 14:17:02 -0800

    fix elint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup (firstName, lastName, filename) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(filename)
+    uploadPhoto(filename),
   ])
     .then((results) => (results));
 }
