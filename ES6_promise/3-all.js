6c7dbcafe560dd651016e7e1e53e580d2f94d6fa
LuciaPuppo897 <alerome897@gmail.com>
2024-01-27 15:32:27 -0800

    change permission

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   Promise.all([
     uploadPhoto(),
     createUser(),
