7790ff6d7e906c8e206684404723b73db0ef03d7
LuciaPuppo897 <alerome897@gmail.com>
2024-02-02 22:56:34 -0800

    fixed task 2 and task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(groceryMap) {
+  if (!(map instanceof Map)) throw new Error('cannont process');
   for (const [item, quantity] of groceryMap.entries()) {
     if (quantity === 1) {
       groceryMap.set(item, 100);
