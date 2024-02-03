9a92bde483565dc78da4a61b04fb9cd6b69ba436
LuciaPuppo897 <alerome897@gmail.com>
2024-02-02 22:29:37 -0800

    fixed task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    throw new Error();
+  }
+
   const filteredValues = Array.from(set).filter(value => value.startsWith(startString)).map(value => value.slice(startString.length));
 
   return filteredValues.join('-');
