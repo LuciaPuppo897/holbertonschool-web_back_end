6ed1d0603e65615c6a74985928d687a1ad26b0fd
LuciaPuppo897 <alerome897@gmail.com>
2024-02-02 22:10:27 -0800

    add task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const studentsInCity = students.filter(student => student.location === city);
+
+  const updatedStudents = studentsInCity.map(student => {
+    const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+
+    const grade = matchingGrade ? matchingGrade.grade : 'N/A';
+    return { ...student, grade };
+  });
+
+  return updatedStudents;
+}
