b62d7f4026308021ee25c66abf37e066f869ed5c
LuciaPuppo897 <alerome897@gmail.com>
2024-02-11 21:23:51 -0800

    fixed elint and add task 6

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,48 +1,50 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name !== 'string') {
-            throw new Error('Name must be a string');
-        }
-        if (typeof length !== 'number') {
-            throw new Error('Length must be a number');
-        }
-        if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
-            throw new Error('Students must be an array of strings');
-        }
-
-        this._name = name;
-        this._length = length;
-        this._students = students;
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+      throw new Error('Students must be an array of strings');
     }
 
-    get name() {
-        return this._name;
-    }
-    set name(newName) {
-        if (typeof newName !== 'string') {
-            throw new Error('Name must be a string');
-        }
-        this._name = newName;
-    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get length() {
-        return this._length;
-    }
-    set length(newLength) {
-        if (typeof newLength !== 'number') {
-            throw new Error('Length must be a number');
-        }
-        this._length = newLength;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get students() {
-        return this._students;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
     }
-    set students(newStudents) {
-        if (!Array.isArray(newStudents) || !newStudents.every(s => typeof s === 'string')) {
-            throw new Error('Students must be an array of strings');
-        }
-        this._students = newStudents;
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number');
     }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every(s => typeof s === 'string')) {
+      throw new Error('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
 }
-
--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (
+      this.constructor !== Building && !this - this.evacuationWarningMessage) 
+      { throw new Error('Class extending Building must override evacuationWarningMessage'); }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
